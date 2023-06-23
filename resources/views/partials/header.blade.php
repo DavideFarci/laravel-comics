816ee3541051dac081ca4f772262142f744320f5
DavideFarci <yank92.df@gmail.com>
2023-06-23 16:16:47 +0200

    primo commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+
+<header>
+    <div class="bg_header">
+    <img src="../assets/img/dc-logo.png" alt="logo-DC" />
+    <AppMenu :menuItems="items" />
+    </div>
+    <div class="jumbotron">
+    <button><a href="#">CURRENT SERIES</a></button>
+    </div>
+</header>
